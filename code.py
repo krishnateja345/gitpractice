3dc0c603359e6ee496aa14d5c078e4f6f4103a11
unknown <krishnateja345@gmail.com>
2024-09-17 21:42:07 +0530

    Committing code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
 print('this is sample python code.')
+
+def format_name(f_name,l_name):
+    formated_fname=f_name.title()
+    formated_lname=l_name.title()
+    print(f"{formated_fname} {formated_lname}")
+
+format_name("krishna","TEja") 
